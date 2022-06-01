d5a66caa54d4afe7ab0199ce2bf721376ed08746
Nicholas Lever <nik@catalystpics.co.uk>
2022-06-01 16:59:12 +0100

    update

--- start/lecture5/app.js
+++ start/lecture5/app.js
@@ -11,6 +11,13 @@ class App{
         
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0xaaaaaa );
+
+		const ambient = new THREE.HemisphereLight( 0xFFFFFF, 0xBBBBFF, 0.3);
+		this.scene.add( ambient );
+
+		const light = new THREE.DirectionalLight();
+		light.position.set( 0.2, 1, 1);
+		this.scene.add( light );
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
@@ -18,15 +25,27 @@ class App{
 		container.appendChild( this.renderer.domElement );
 		       
         this.renderer.setAnimationLoop(this.render.bind(this));
+
+		const geometry = new THREE.BoxGeometry();
+		const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
+
+		this.mesh = new THREE.Mesh( geometry, material );
+
+		const controls = new OrbitControls( this.camera, this.renderer.domElement );
+
+		this.scene.add( this.mesh );
     
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
-
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
     }
     
-	render( ) {   
+	render( ) {  
+		this.mesh.rotateX( 0.01 ); 
 		this.renderer.render( this.scene, this.camera );
     }
 }
