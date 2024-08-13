07cd8dabb943fb1a5a2577fa16a3ed78d2d146e2
Nik Lever <47000827+NikLever@users.noreply.github.com>
2024-08-13 08:21:28 +0100

    Removed outputEncoding from lec9

--- start/lecture9/app.js
+++ start/lecture9/app.js
@@ -26,7 +26,6 @@ class App{
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
         container.appendChild( this.renderer.domElement );
 		this.setEnvironment();
 		
